30d56a4214d42958b5f415b51e188c74b6c545ba
XiaoBo <chenxiaobo@xmfunny.com>
2023-11-28 18:49:49 +0800

    mics: 新增GooglePay 构建流程代码

---
+++ Editor/Config/FunnyIAPTarget.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SoFunny.FunnySDK.IAP.Editor
+{
+    /// <summary>
+    /// IAP 服务渠道目标
+    /// </summary>
+    public enum FunnyIAPTarget
+    {
+        /// <summary>
+        /// 国内
+        /// </summary>
+        Mainland,
+
+        /// <summary>
+        /// 海外
+        /// </summary>
+        Oversea
+    }
+}
+
